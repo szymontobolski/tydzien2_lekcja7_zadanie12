1c10a2c6d753776d9c0719eb3e32f84954fd50f8
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-22 12:54:37 +0200

    Add project files.

---
+++ tydzien2_lekcja7_zadanie12/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace tydzien2_lekcja7_zadanie12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj numer dnia tygodnia");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("poniedziałek");
+                    break;
+                case 2:
+                    Console.WriteLine("wtorek");
+                    break;
+                case 3:
+                    Console.WriteLine("środa");
+                    break;
+                case 4:
+                    Console.WriteLine("czwartek");
+                    break;
+                case 5:
+                    Console.WriteLine("piątek");
+                    break;
+                case 6:
+                    Console.WriteLine("sobota");
+                    break;
+                case 7:
+                    Console.WriteLine("niedziela");
+                    break;
+             }
+            
+            
+
+
+
+
+        }
+    }
+}
